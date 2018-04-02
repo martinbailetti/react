31266a4fca8955a632c3d53532c22a1e401bfa13
Martin <martinbailetti@gmail.com>
2018-04-01 21:24:02 -0500

    v2

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ import Media from './src/playlist/components/media';
 const app = document.getElementById('app');
 
 
-ReactDOM.render(<Media />, app);
+ReactDOM.render(<Media title="Mi título" author="Martín Bailetti" image="./images/covers/responsive.jpg" />, app);
